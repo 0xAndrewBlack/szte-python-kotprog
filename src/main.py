1b2fe5476ae897d080e22e60b5c19ff917a9ab2a
Lemon <andras@fekete.io>
2023-04-16 18:32:49 +0200

    🧩 feat: Extend the BOT. (#4)
    * 🧩 feat: Discord Interaction API implemented.
    
    * ✨ feat: Logging and error handling.

--- src/main.py
+++ src/main.py
@@ -1,25 +1,81 @@
+import os
+import random
 import discord
-from decouple import config
+import logging
 
-intents = discord.Intents.default()
-intents.message_content = True
+from typing import Literal, Union, NamedTuple
+from discord import app_commands
+from decouple import config
+from enum import Enum
 
 BOT_TOKEN = config('DC_BOT_TOKEN')
+MY_GUILD = discord.Object(id=config('GUILD_ID', cast=int))
 
-client = discord.Client(intents=intents)
+discord.utils.setup_logging()
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+logging.getLogger('discord.http').setLevel(logging.DEBUG)
+
+
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+
+        await self.tree.sync(guild=MY_GUILD)
+
+
+client = MyClient()
 
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    logger.info(f'Logged in as {client.user} (ID: {client.user.id})')
+    logger.info('-'*50)
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.tree.command()
+@app_commands.describe()
+async def roll(interaction: discord.Interaction):
+    """Rolls a dice"""
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    await interaction.response.send_message(f'You rolled a {random.randint(1, 6)}', ephemeral=True)
 
-client.run(BOT_TOKEN)
+
+@client.tree.command()
+@app_commands.describe(first='The first number to add', second='The second number to add')
+async def add(interaction: discord.Interaction, first: app_commands.Range[int, 0, 100], second: app_commands.Range[int, 0, None],):
+    """Adds two numbers together"""
+
+    await interaction.response.send_message(f'{first} + {second} = {first + second}', ephemeral=True)
+
+
+@client.tree.command(name='channel-info')
+@app_commands.describe(channel='The channel to get info of')
+async def channel_info(interaction: discord.Interaction, channel: Union[discord.VoiceChannel, discord.TextChannel]):
+    """Shows basic channel info for a text or voice channel."""
+
+    embed = discord.Embed(title='Channel Info')
+    embed.add_field(name='Name', value=channel.name, inline=True)
+    embed.add_field(name='ID', value=channel.id, inline=True)
+    embed.add_field(name='Type', value='Voice' if isinstance(channel, discord.VoiceChannel) else 'Text', inline=True)
+
+    embed.set_footer(text='Created').timestamp = channel.created_at
+
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+
+@client.tree.command()
+@app_commands.describe(action='The action to do in the shop', item='The target item')
+async def shop(interaction: discord.Interaction, action: Literal['Buy', 'Sell'], item: str):
+    """Interact with the shop"""
+
+    await interaction.response.send_message(f'Action: {action}\nItem: {item}')
+
+
+client.run(BOT_TOKEN, log_handler=handler, log_level=logging.DEBUG)
